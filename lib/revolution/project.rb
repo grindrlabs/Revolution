85c641b4201dac040cb68f1d312e20f94f08e285
Shelby <shelbyspees@gmail.com>
2017-11-01 15:54:20 -0700

    Refactor project module to simplify testing

--- lib/revolution/project.rb
+++ lib/revolution/project.rb
@@ -5,91 +5,98 @@ require 'json'
 require 'open3'
 require 'fpm/cookery/recipe'
 
-def inspect_recipe(path)
-  recipe_json = nil
-  data = nil
+module Projects
+  # Takes in path of directory containing all packages
+  # Returns a list of Project objects populated with project and package data
+  def self.load_projects(base_path)
+    projects = []
 
-  cmd = 'fpm-cook inspect ' + path
-  Open3.popen3(cmd) do |stdin, stdout, stderr, wait_thr|
-    pid = wait_thr.pid
-    stdin.close
-    recipe_json = stdout.read
-    err         = stderr.read
-    status      = wait_thr.value
-    puts 'ERROR: Invalid recipe.rb file!' unless status.exitstatus.zero?
+    # Get subdirectories immediately inside of base_path
+    recipe_dirs = []
+    root        = Dir.pwd
+    if Dir.exist?(base_path)
+      Dir.chdir(base_path)
+      recipe_dirs = Dir.glob('*/')
+      Dir.chdir(root)
+    end
+
+    # Create Project object for each recipe_dir
+    recipe_dirs.each do |subpath|
+      path = base_path + subpath
+      projects.push(Project.new(path)) if File.exist?(path + '/recipe.rb')
+    end
+    projects
   end
 
-  begin
-    data = JSON.parse(recipe_json)
-  rescue JSON::ParserError
-    # json_inspect should only be called when there's a valid recipe file
-    # but this is here for testing
-    puts 'ERROR: Invalid JSON!'
-    return nil
-  end
-  data
-end
+  # Stores data for a single recipe
+  # One recipe has one or more target packages
+  class Project
+    attr_accessor :name, :data, :packages
 
-# Takes in path of directory containing all packages
-# Returns a list of Project objects populated with project and package data
-def load_projects(base_path)
-  projects    = []
+    def initialize(path)
+      @data = Projects.inspect_recipe(path)
 
-  # Get subdirectories immediately inside of base_path
-  recipe_dirs = []
-  root        = Dir.pwd
-  if Dir.exist?(base_path)
-    Dir.chdir(base_path)
-    recipe_dirs = Dir.glob('*/')
-    Dir.chdir(root)
-  end
+      @packages = []
+      if chain_package?
+        # TODO
+        # for each r in chain_recipes
+        #   create a package object with appropriate @data
+      else
+        @packages.push(Package.new(path))
+      end
+    end
 
-  # Create Project object for each recipe_dir
-  recipe_dirs.each do |subpath|
-    path = base_path + subpath
-    projects.push(Project.new(path)) if File.exist?(path + '/recipe.rb')
-  end
-  projects
-end
+    def chain_package?
+      @data['chain_package']
+    end
 
-class Project
-  attr_accessor :name, :data, :packages
-
-  def initialize(path)
-    @data = inspect_recipe(path)
-
-    @packages = []
-    if chain_package?
-      # TODO
-      # for each r in chain_recipes
-      #   create a package object with appropriate @data
-    else
-      @packages.push(Package.new(path))
+    def chain_recipes
+      @data['chain_recipes'] if chain_package?
     end
   end
 
-  def chain_package?
-    @data['chain_package']
+  # Stores data for a single target package and its dependencies
+  class Package
+    attr_accessor :name, :data, :recipe, :project
+    attr_accessor :dependencies
+
+    def initialize(path)
+      @data         = Projects.inspect_recipe(path)
+      @name         = @data['name']
+      @recipe       = path + '/recipe.rb'
+      @dependencies = @data['depends']
+    end
+
+    def depends?
+      return false if dependencies.empty?
+      true
+    end
   end
 
-  def chain_recipes
-    @data['chain_recipes'] if chain_package?
+  def self.inspect_recipe(path)
+    recipe_json = nil
+    data        = nil
+
+    cmd = 'fpm-cook inspect ' + path
+    Open3.popen3(cmd) do |stdin, stdout, stderr, wait_thr|
+      pid = wait_thr.pid
+      stdin.close
+      recipe_json = stdout.read
+      err         = stderr.read
+      status      = wait_thr.value
+      puts 'ERROR: Invalid recipe.rb file!' unless status.exitstatus.zero?
+    end
+
+    begin
+      data = JSON.parse(recipe_json)
+    rescue JSON::ParserError
+      # method should only be called when there's a valid recipe file
+      # but this is here for testing
+      puts 'ERROR: Invalid JSON!'
+      return nil
+    end
+    data
   end
+
 end
 
-class Package
-  attr_accessor :name, :data, :recipe, :project
-  attr_accessor :dependencies
-
-  def initialize(path)
-    @data         = inspect_recipe(path)
-    @name         = @data['name']
-    @recipe       = path + '/recipe.rb'
-    @dependencies = @data['depends']
-  end
-
-  def depends?
-    return false if dependencies.empty?
-    true
-  end
-end
--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -8,13 +8,13 @@ describe 'inspect_recipe' do
   context 'when passed a valid base directory containing recipe directories' do
     it 'returns hash object containing recipe data' do
       path = 'examples/golang'
-      expect(inspect_recipe(path)).to be_a Hash
+      expect(Projects.inspect_recipe(path)).to be_a Hash
     end
   end
   context 'when given an invalid package directory' do
     it 'returns nil' do
       path = Dir.pwd
-      expect(inspect_recipe(path)).to be nil
+      expect(Projects.inspect_recipe(path)).to be nil
     end
   end
 end
@@ -22,26 +22,26 @@ end
 describe 'load_projects' do
   context 'when passed a valid directory containing recipes' do
     before(:all) do
-      @projects = load_projects('examples/')
+      @projects = Projects.load_projects('examples/')
     end
     it 'returns an array' do
       expect(@projects).to be_an Array
     end
     it 'returns an array of one or more Project objects' do
-      expect(@projects[0]).to be_a Project
+      expect(@projects[0]).to be_a Projects::Project
     end
   end
 end
 
-describe 'Project' do
+describe 'Projects' do
   context 'project with one recipe' do
     before(:all) do
-      @proj = Project.new('examples/golang')
+      @proj = Projects::Project.new('examples/golang')
     end
 
     describe '#initialize' do
       it 'returns a Project object' do
-        expect(@proj).to be_a Project
+        expect(@proj).to be_a Projects::Project
       end
       it 'populates @data' do
         expect(@proj.data['name']).not_to be nil
@@ -56,12 +56,12 @@ describe 'Project' do
 
   context 'project with multiple recipes' do
     before(:all) do
-      @proj = Project.new('examples/project-chain-recipes')
+      @proj = Projects::Project.new('examples/project-chain-recipes')
     end
 
     describe '#initialize' do
       it 'returns a Project object' do
-        expect(@proj).to be_a Project
+        expect(@proj).to be_a Projects::Project
       end
       it 'populates @data' do
         expect(@proj.data['name']).not_to be nil
@@ -79,12 +79,12 @@ end
 describe 'Package' do
   context 'package with no dependencies' do
     before(:all) do
-      @go = Package.new('examples/golang')
+      @go = Projects::Package.new('examples/golang')
     end
 
     describe '#initialize' do
       it 'returns a Package object' do
-        expect(@go).to be_a Package
+        expect(@go).to be_a Projects::Package
       end
     end
     describe '#depends?' do
@@ -96,12 +96,12 @@ describe 'Package' do
 
   context 'package with dependencies' do
     before(:all) do
-      @pkg = Package.new('examples/package-with-deps')
+      @pkg = Projects::Package.new('examples/package-with-deps')
     end
 
     describe '#initialize' do
       it 'returns a Package object' do
-        expect(@pkg).to be_a Package
+        expect(@pkg).to be_a Projects::Package
       end
     end
     describe '#depends?' do
