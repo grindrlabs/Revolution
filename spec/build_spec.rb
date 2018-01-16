a4eb58399d1888a1b36405b05728fc95002ef4c2
Shelby <shelbyspees@gmail.com>
2018-01-16 08:55:27 -0800

    Improve build tests and eliminate redundancy, fix RPM building in Travis
    - Specify CentOS 7 target in build command
    - Install rpm on Travis

--- spec/build_spec.rb
+++ spec/build_spec.rb
@@ -2,16 +2,30 @@
 
 require 'rspec'
 require 'revolution/build'
+require 'revolution/recipes'
 
 RSpec.describe 'Build' do
   context 'using an example package' do
     let(:pkg_path) { 'examples/golang' }
     let(:recipe_root) { 'examples' }
+    let(:result) { Build.build_package(pkg_path) }
+    let(:rpms) { Dir.glob(pkg_path + '/pkg/*.rpm') }
+    let(:recipe) { Recipes::Recipe.new(File.join(pkg_path, '/recipe.rb')) }
+    let(:version) { recipe.data['version'] }
+    let(:cleanup) { Build.remove_package(pkg_path) }
 
     describe '.build_package' do
-      it 'returns successfully'
-      it 'creates an .rpm'
-      it 'creates an .rpm with the correct version number'
+      it 'returns successfully' do
+        expect(result).to be 0
+      end
+      it 'creates an .rpm' do
+        expect(rpms).to be_an Array
+        expect(rpms.length).to be 1
+        expect(rpms[0]).to end_with('.rpm')
+      end
+      it 'creates an .rpm with the correct version number' do
+        expect(rpms[0].include?(version)).to be true
+      end
     end
 
     describe '.install_build_deps' do
@@ -50,6 +64,7 @@ RSpec.describe 'Build' do
 
       it 'removes cache/ subdirectory' do
         expect(Dir.exist?(cache)).to be false
+        cleanup
       end
     end
   end
