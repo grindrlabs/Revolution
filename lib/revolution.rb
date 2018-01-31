6644bdac3a1d1d7650be353450ed9bb6177d61b4
Shelby <shelbyspees@gmail.com>
2018-01-31 12:17:19 -0800

    Change `check` to `valid?` tests, rename for clarity in entry point

--- lib/revolution.rb
+++ lib/revolution.rb
@@ -10,108 +10,105 @@ require 'revolution/rpm_sign'
 require 'thor'
 
 module Revolution
-  def self.sort_recipes(root)
-    check_dir(root)
-    recipes        = Recipes.load_recipes(root)
+  def self.sort_recipes(recipe_root)
+    puts 'Getting build order...' if valid_dir?(recipe_root)
+    recipes        = Recipes.load_recipes(recipe_root)
     sorted_recipes = Order.resolve_build_order(recipes)
     puts 'Recipes will build in the following order:'
     sorted_names = sorted_recipes.map(&:package_name)
-    output_pkg_list(sorted_names)
+    print_packages(sorted_names)
     sorted_recipes
   end
 
-  def self.build_all(root)
-    check_dir(root)
-    puts 'Building packages in order...'
-    sorted_recipes = sort_recipes(root)
+  def self.build_all(recipe_root)
+    puts 'Building packages in order...' if valid_dir?(recipe_root)
+    sorted_recipes = sort_recipes(recipe_root)
     built_packages = sorted_recipes.map do |recipe|
-      pkg_name = recipe.package_name
-      build_one(root, pkg_name)
-      pkg_name
+      package_name = recipe.package_name
+      build_one(recipe_root, package_name)
+      package_name
     end
     puts 'Successfully built the following packages:'
-    output_pkg_list(built_packages)
+    print_packages(built_packages)
   end
 
-  def self.build_one(root, pkg_name)
-    package_path = File.join(root, pkg_name)
-    check_pkg(package_path)
-    puts "Building package: #{pkg_name}"
-    Build.remove_package(package_path)
-    Build.build_package(package_path)
-    Build.clean(package_path)
+  def self.build_one(recipe_root, package_name)
+    recipe_dir = File.join(recipe_root, package_name)
+    puts "Building package: #{package_name}" if valid_recipe_dir?(recipe_root)
+    Build.remove_package(recipe_dir)
+    Build.build_package(recipe_dir)
+    Build.clean(recipe_dir)
   end
 
-  def self.clean_all(root)
-    check_dir(root)
-    puts 'Removing packages...'
-    recipes          = Recipes.load_recipes(root)
+  def self.clean_all(recipe_root)
+    puts 'Removing packages...' if valid_dir?(recipe_root)
+    recipes          = Recipes.load_recipes(recipe_root)
     cleaned_packages = recipes.map do |recipe|
-      pkg_name = recipe.package_name
-      clean_one(root, pkg_name)
-      pkg_name
+      package_name = recipe.package_name
+      clean_one(recipe_root, package_name)
+      package_name
     end
     puts 'Successfully removed the following packages:'
-    output_pkg_list(cleaned_packages)
+    print_packages(cleaned_packages)
   end
 
-  def self.clean_one(root, pkg_name)
-    package_path = File.join(root, pkg_name)
-    check_pkg(package_path)
-    puts "Removing #{pkg_name}..."
-    Build.remove_package(package_path)
-    puts "Successfully removed package and temp dirs for #{pkg_name}"
+  def self.clean_one(recipe_root, package_name)
+    recipe_dir = File.join(recipe_root, package_name)
+    puts "Removing #{package_name}..." if valid_recipe_dir?(recipe_dir)
+    Build.remove_package(recipe_dir)
+    puts "Successfully removed package and temp dirs for #{package_name}"
   end
 
-  def self.deploy(config_file, root)
-    puts 'Starting deployment...'
+  def self.deploy(config_file, recipe_root)
+    puts 'Starting deployment...' if valid_dir?(recipe_root)
     bucket  = RPMRepository.get_location(config_file)
     manager = RPMRepository::Manager.new(bucket: bucket)
     manager.fetch_repository
-    manager.copy_packages(root)
+    manager.copy_packages(recipe_root)
     manager.update_metadata
     manager.upload_repository
     manager.cleanup
   end
 
-  def self.sign_all(root)
-    pkg_dirs = Dir.glob(root + '/*/pkg/')
-    pkg_dirs.each do |pkg_dir|
-      check_dir(pkg_dir)
+  def self.sign_all(recipe_root)
+    built_package_dirs = Dir.glob(recipe_root + '/*/pkg/')
+    built_package_dirs.each do |built_package_dir|
+      valid_dir?(built_package_dir)
     end
-    puts 'Signing packages...'
-    rpms = Dir.glob(root + '/*/pkg/*.rpm')
+    puts 'Signing packages...' if valid_dir?(recipe_root)
+    rpms = Dir.glob(recipe_root + '/*/pkg/*.rpm')
     rpms.each do |rpm|
       rpm_path = File.expand_path(rpm)
-      check_rpm(rpm_path)
-      sign_one(rpm_path)
+      sign_one(rpm_path) if valid_rpm_path?(rpm_path)
     end
   end
 
   def self.sign_one(rpm_path)
     rpm_path = File.expand_path(rpm_path)
-    check_rpm(rpm_path)
-    puts "Signing #{rpm_path}..."
+    puts "Signing #{rpm_path}..." if valid_rpm_path?(rpm_path)
     RPMSign.addsign(rpm_path)
     raise Error::InvalidSignature unless RPMSign.signed?(rpm_path)
   end
 
-  def self.check_dir(path)
+  def self.valid_dir?(path)
     error_msg = "Cannot find directory: #{path}"
     raise Error::InvalidDirectory, error_msg unless Dir.exist?(path)
+    true
   end
 
-  def self.check_pkg(path)
-    error_msg = "Cannot find package at filepath: #{path}"
-    raise Error::InvalidPackageName, error_msg unless Dir.exist?(path)
+  def self.valid_recipe_dir?(path)
+    error_msg = "Cannot find recipe directory: #{path}"
+    raise Error::InvalidDirectory, error_msg unless Dir.exist?(path)
+    true
   end
 
-  def self.check_rpm(path)
+  def self.valid_rpm_path?(path)
     basename = File.basename(path)
     raise Error::InvalidRPM, "#{basename} not found" unless File.exist?(path)
+    true
   end
 
-  def self.output_pkg_list(list)
+  def self.print_packages(list)
     list.each { |item| puts ''.ljust(10) + item }
   end
 end
--- spec/revolution_spec.rb
+++ spec/revolution_spec.rb
@@ -7,23 +7,23 @@ require 'revolution/exceptions'
 RSpec.describe Revolution do
   describe '.check_dir?' do
     it 'does not raise an error if directory exists' do
-      expect { Revolution.check_dir('examples/') }.not_to raise_error
+      expect { Revolution.valid_dir?('examples/') }.not_to raise_error
     end
     it 'raises error if directory does not exist' do
       expect do
-        Revolution.check_dir('fake/')
+        Revolution.valid_dir?('fake/')
       end.to raise_error(Revolution::Error::InvalidDirectory)
     end
   end
 
   describe '.check_pkg?' do
     it 'does not raise an error if package directory exists' do
-      expect { Revolution.check_pkg('examples/golang') }.not_to raise_error
+      expect { Revolution.valid_recipe_dir?('examples/golang') }.not_to raise_error
     end
     it 'raises error if package directory does not exist' do
       expect do
-        Revolution.check_pkg('examples/test')
-      end.to raise_error(Revolution::Error::InvalidPackageName)
+        Revolution.valid_recipe_dir?('examples/test')
+      end.to raise_error(Revolution::Error::InvalidDirectory)
     end
   end
 end
